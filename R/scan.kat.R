f1593c6d5c864fc8d88896018cd2462a4617b2fa
Malarkey73 <s.henderson@ucl.ac.uk>
2014-10-03 15:41:44 +0100

    fixed scan.kat

--- R/scan.kat.R
+++ R/scan.kat.R
@@ -1,17 +1,17 @@
-scan.kat<- function(VCF, win=3, max.threshold =500)
+scan.kat<- function(VCF, win=5, max.threshold =500)
 {
   library(RcppRoll)
 
   # add a rolling median window
-  kat<- VCF %>%
+  rmax<- VCF %>%
     group_by(chr) %>%
     mutate(intermutation = start.position-lag(start.position)) %>%
-    do(., data.frame(roll_max=c(roll_max(.$intermutation, n=win), rep(NA, win-2))))
+    do(., data.frame(roll_max=c(roll_max(.$intermutation, n=win), rep(NA, win-1))))
 
   
 
   # label poinst below that threshold TRUE
-  kat<- mutate(kat, below.mth = roll_max < max.threshold)
+  kat<- mutate(VCF, below.mth = rmax$roll_max < max.threshold)
 
   bmth <- kat$below.mth
   burst.gr<-0
@@ -20,22 +20,26 @@ scan.kat<- function(VCF, win=3, max.threshold =500)
   
   for(i in 1:length(bmth))
   {
-    if(bmth[i]==FALSE| is.na(bmth[i]))
+    if(bmth[i]==FALSE | is.na(bmth[i]))
     {
       vec[i]<-0
       last<- FALSE
     }
-    if(bmth[i]==TRUE & last==FALSE | !isna(bmth[i]))
-    {
-      burst.gr<- burst.gr +1
-      last<-TRUE
-      vec[i]<- burst.gr
-    }
-    if(bmth[i]==TRUE & last==TRUE | !isna(bmth[i]))
+    
+    if(bmth[i]==TRUE & last==FALSE & !is.na(bmth[i]))
     {
       burst.gr<- burst.gr +1
       last<-TRUE
       vec[i]<- burst.gr
     }
     
+    if(bmth[i]==TRUE & last==TRUE & !is.na(bmth[i]))
+    {
+      vec[i]<- burst.gr
+    }
+    
   }
+  return(mutate(kat, bursts=as.factor(vec)))
+  
+}
+
