a03c6e8fdb88bf6b707ef464fbc32b119f9105d8
Malarkey73 <s.henderson@ucl.ac.uk>
2014-10-06 13:44:04 +0100

    a working first version of stat.kat

--- R/stat.kat.R
+++ R/stat.kat.R
@@ -1,7 +1,27 @@
-stat.kat <- function(VCF, bursts="bursts")
+stat.kat <- function(VCF, bursts="bursts", intra.burst=T)
 {
   kat<- VCF %>%
-    group_by(bursts) %>%
+    group_by(bursts, chr) %>%
+    mutate(intermutation=lead(start.position)-start.position) %>%
+    summarise(burst.start= min(start.position),
+              burst.end=max(start.position),
+              burst.length= burst.end-burst.start,
+              n=n(),
+              min.gap = min(intermutation, na.rm=T),
+              q25.gap = quantile(intermutation, 0.25, na.rm=T),
+              median.gap = median(intermutation,na.rm=T),
+              mean.gap = mean(intermutation,na.rm=T),
+              q75.gap = quantile(intermutation, 0.25, na.rm=T),
+              max.gap = max(intermutation,na.rm=T))
+           
+  if(intra.burst==TRUE){
+    kat <- kat %>%
+      group_by(chr) %>%
+      mutate(intra.burst= lead(burst.start)-burst.end) %>%
+      filter(bursts != 0)
     
-    mutate(fivenum(bursts))
+  }
+  
+  return(kat)
+   
 }
